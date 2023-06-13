39078138c7c83a6f0a57edcef2edff40c2f98b6a
Fayaz <fana.fayaz@gmail.com>
2023-06-13 07:32:19 +0530

    Changed variable names

--- Addition.java
+++ Addition.java
@@ -2,10 +2,10 @@ class Addition
 {
 	public static void main(String args[])
 	{
-		int n1,n2,sum=0;
-		n1=10;
-		n2=35;
-		sum=n1+n2;
+		int num1,num2,sum=0;
+		num1=10;
+		num2=35;
+		sum=num1+num2;
 		System.out.println("The sum value is : "+sum);
 	}
 }
