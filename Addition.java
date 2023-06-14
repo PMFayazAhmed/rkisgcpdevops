579c88334a68dc04117c579e063b9904874b7619
Fayaz <fana.fayaz@gmail.com>
2023-06-14 07:49:44 +0530

    Revert "Modified variable names"
    This reverts commit 4bc58eecb1e5809fdf82f2bacff684593ac382c5.

--- Addition.java
+++ Addition.java
@@ -2,10 +2,10 @@ class Addition
 {
 	public static void main(String args[])
 	{
-		int number1,number2,sum=0;
-		number1=10;
-		number2=35;
-		sum=number1+number2;
+		int n1,n2,sum=0;
+		n1=10;
+		n2=35;
+		sum=n1+n2;
 		System.out.println("The sum value is : "+sum);
 		System.out.println("This is the last statement");
 	}
